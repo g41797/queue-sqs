2f00145e45896cb3d6ecd0f625e67f96afa60eb9
g41797 <g41797@gmail.com>
2024-06-12 12:58:57 +0300

    Prepare LocalStack environment

--- docker/start.sh
+++ docker/start.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
+date
+sudo apt install python3-pip
+date
 curl -Lo localstack-cli-3.4.0-linux-amd64-onefile.tar.gz \
     https://github.com/localstack/localstack-cli/releases/download/v3.4.0/localstack-cli-3.4.0-linux-amd64-onefile.tar.gz
 sudo tar xvzf localstack-cli-3.4.0-linux-*-onefile.tar.gz -C /usr/local/bin
 localstack --version
+date
 export LOCALSTACK_AUTH_TOKEN="ls-cIpo9773-Vaku-guFE-1697-DaBUyEwu01b9"
 export LOCALSTACK_SERVICEES="SQS"
 localstack start -d
-sudo apt install python3-pip
-
+date
 # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 # unzip awscliv2.zip
 # sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
 # which aws
 # ls -l /usr/local/bin/aws
 # aws --version
-
+date
 pip install awscli-local
 awslocal help
+date
 
