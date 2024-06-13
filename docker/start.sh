105f81e535fb0cb5d4ac1a58589b8d856d55bfaf
g41797 <g41797@gmail.com>
2024-06-13 11:24:16 +0300

    Add installation of aws cli v2

--- docker/start.sh
+++ docker/start.sh
@@ -5,25 +5,35 @@ date
 curl -Lo localstack-cli-3.4.0-linux-amd64-onefile.tar.gz \
     https://github.com/localstack/localstack-cli/releases/download/v3.4.0/localstack-cli-3.4.0-linux-amd64-onefile.tar.gz
 sudo tar xvzf localstack-cli-3.4.0-linux-*-onefile.tar.gz -C /usr/local/bin
+rm -f localstack-cli-3.4.0-linux-*-onefile.tar.gz
 localstack --version
 date
-export LOCALSTACK_AUTH_TOKEN="ls-cIpo9773-Vaku-guFE-1697-DaBUyEwu01b9"
+export LOCALSTACK_AUTH_TOKEN="ls-soyUdUBu-fODo-zAZE-XOlO-9157WULu09eb"
 export LOCALSTACK_SERVICEES="sqs"
 export LOCALSTACK_SQS_ENDPOINT_STRATEGY="path"
 localstack start -d
 date
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
-# which aws
-# ls -l /usr/local/bin/aws
-# aws --version
+sudo apt remove awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -u awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+which aws
+ls -l /usr/local/bin/aws
+aws --version
+rm -f awscliv2.zip
 date
 pip install awscli-local
+pip install awscli
 date
+pwd
 echo Create Queue
-awslocal sqs create-queue --queue-name yii-queue
+awslocal sqs create-queue --queue-name test-queue.fifo --attributes FifoQueue=true
+echo List Queues
+awslocal sqs list-queues
+echo Delete Queue
+awslocal sqs delete-queue --queue-url "http://localhost.localstack.cloud:4566/queue/us-east-1/000000000000/test-queue.fifo"
 echo List Queues
 awslocal sqs list-queues
 date
+pwd
 
