7bc3b74820301fdd4952931ee96c2c3413288db2
g41797 <g41797@gmail.com>
2024-06-21 18:04:07 +0300

    Fix ext-zip installation

--- docker/start.sh
+++ docker/start.sh
@@ -49,7 +49,7 @@ echo
 echo Ininstall aws utilities
 echo
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -u awscliv2.zip
+unzip -qq -u awscliv2.zip
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
 which aws
 ls -l /usr/local/bin/aws
